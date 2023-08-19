54e5cb0edb523638a49b92df0bfe5c7e56c82545
AndreAshmeade <andrelashmeade@gmail.com>
2023-08-19 14:15:49 -0400

    .

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import NewTodoform from "./NewTodoform";
+import TodoList from "./TodoList";
+import "./style.css";
+
+export default function App() {
+  // Always put hooks at the top of the file
+  //State is immutable (state holds todo array data)
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+
+    return JSON.parse(localValue);
+  });
+
+  //Store
+  //Cannot have conditional hooks
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ];
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <NewTodoform onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  );
+}
